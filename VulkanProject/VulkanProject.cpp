3b2b453423a207ae74d05dae5a92ab8e75dc4c8a
BobsIsHere <maritte.kindt@student.howest.be>
2025-02-18 22:49:39 +0100

    Presentation Querying Details of Swap Chain Support

--- VulkanProject/VulkanProject.cpp
+++ VulkanProject/VulkanProject.cpp
@@ -51,6 +51,13 @@ private:
         }
     };
 
+    struct SwapChainSupportDetails 
+    {
+        VkSurfaceCapabilitiesKHR capabilities;
+        std::vector<VkSurfaceFormatKHR> formats;
+        std::vector<VkPresentModeKHR> presentModes;
+    };
+
     // Member Functions
     void initWindow() 
     {
@@ -275,7 +282,35 @@ private:
     bool isDeviceSuitable(VkPhysicalDevice device) 
     {
         QueueFamilyIndices indices = findQueueFamilies(device);
-        return indices.isComplete(); 
+
+        bool extensionsSupported = checkDeviceExtensionSupport(device);
+
+        bool swapChainAdequate = false;
+        if (extensionsSupported) 
+        {
+            SwapChainSupportDetails swapChainSupport = querySwapChainSupport(device);
+            swapChainAdequate = !swapChainSupport.formats.empty() && !swapChainSupport.presentModes.empty();
+        }
+
+        return indices.isComplete() && extensionsSupported && swapChainAdequate; 
+    }
+
+    bool checkDeviceExtensionSupport(VkPhysicalDevice device) 
+    {
+        uint32_t extensionCount; 
+        vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties> availableExtensions(extensionCount); 
+        vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, availableExtensions.data());
+
+        std::set<std::string> requiredExtensions(deviceExtensions.begin(), deviceExtensions.end()); 
+
+        for (const auto& extension : availableExtensions) 
+        {
+            requiredExtensions.erase(extension.extensionName);
+        }
+
+        return requiredExtensions.empty(); 
     }
 
     QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) 
@@ -341,7 +376,8 @@ private:
         createInfo.pQueueCreateInfos = queueCreateInfos.data();
 
         createInfo.pEnabledFeatures = &deviceFeatures;
-        createInfo.enabledExtensionCount = 0;
+        createInfo.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions.size());
+        createInfo.ppEnabledExtensionNames = deviceExtensions.data();
 
         if (enableValidationLayers) 
         {
@@ -370,6 +406,33 @@ private:
         }
     }
 
+    SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device) 
+    {
+        SwapChainSupportDetails details;
+
+        vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &details.capabilities);
+
+        uint32_t formatCount;
+        vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, nullptr);
+
+        if (formatCount != 0) 
+        {
+            details.formats.resize(formatCount);
+            vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, details.formats.data());
+        }
+
+        uint32_t presentModeCount;
+        vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, nullptr);
+
+        if (presentModeCount != 0) 
+        {
+            details.presentModes.resize(presentModeCount);
+            vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, details.presentModes.data());
+        }
+
+        return details;
+    }
+
     // Member Variables
     GLFWwindow* window;
     VkInstance instance; 
@@ -380,6 +443,11 @@ private:
 
     VkSurfaceKHR surface; 
     VkQueue presentQueue; 
+
+    const std::vector<const char*> deviceExtensions = 
+    {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
 };
 
 int main()
