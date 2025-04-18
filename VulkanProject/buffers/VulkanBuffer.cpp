382a965c0a081cb043a4c1e127490a1af14b880d
BobsIsHere <maritte.kindt@student.howest.be>
2025-04-18 20:02:40 +0200

    Made Buffer base class

---
+++ VulkanProject/buffers/VulkanBuffer.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+#include "VulkanBuffer.h"
+#include "core/VulkanDevice.h"
+
+VulkanBuffer::VulkanBuffer(VulkanDevice* pDevice) :
+	m_pVulkanDevice{ pDevice },
+	m_Buffer{}
+{
+}
+
+VulkanBuffer::~VulkanBuffer()
+{
+}
+
+void VulkanBuffer::Create(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory)
+{
+    VkBufferCreateInfo bufferInfo{};
+    bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    bufferInfo.size = size;
+    bufferInfo.usage = usage;
+    bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+    if (vkCreateBuffer(m_pVulkanDevice->GetDevice(), &bufferInfo, nullptr, &buffer) != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to create buffer!");
+    }
+
+    VkMemoryRequirements memRequirements;
+    vkGetBufferMemoryRequirements(m_pVulkanDevice->GetDevice(), buffer, &memRequirements);
+
+    VkMemoryAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    allocInfo.allocationSize = memRequirements.size;
+    allocInfo.memoryTypeIndex = FindMemoryType(memRequirements.memoryTypeBits, properties);
+
+    if (vkAllocateMemory(m_pVulkanDevice->GetDevice(), &allocInfo, nullptr, &bufferMemory) != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to allocate buffer memory!");
+    }
+
+    vkBindBufferMemory(m_pVulkanDevice->GetDevice(), buffer, bufferMemory, 0);
+}
+
+void VulkanBuffer::Cleanup()
+{
+    vkDestroyBuffer(m_pVulkanDevice->GetDevice(), m_Buffer, nullptr);
+    vkFreeMemory(m_pVulkanDevice->GetDevice(), m_BufferMemory, nullptr);
+}
+
+uint32_t VulkanBuffer::FindMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties)
+{
+    VkPhysicalDeviceMemoryProperties memProperties;
+    vkGetPhysicalDeviceMemoryProperties(m_pVulkanDevice->GetPhysicalDevice(), &memProperties);
+
+    for (uint32_t idx = 0; idx < memProperties.memoryTypeCount; ++idx)
+    {
+        if ((typeFilter & (1 << idx)) && (memProperties.memoryTypes[idx].propertyFlags & properties) == properties)
+        {
+            return idx;
+        }
+    }
+
+    throw std::runtime_error("failed to find suitable memory type!");
+}
---
+++ VulkanProject/buffers/VulkanBuffer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
+
+class VulkanDevice;
+
+class VulkanBuffer
+{
+public:
+	VulkanBuffer(VulkanDevice* pDevice);
+	~VulkanBuffer();
+
+	virtual void Create(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
+	virtual void Cleanup();
+
+private:
+	VulkanDevice* m_pVulkanDevice;
+
+	VkBuffer m_Buffer;
+	VkDeviceMemory m_BufferMemory;
+
+	uint32_t FindMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
+};
